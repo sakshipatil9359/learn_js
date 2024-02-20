7b650ac2606c9ff3310e97b23b8f1ad9d81bebd0
sakshipatil9359 <84892191+sakshipatil9359@users.noreply.github.com>
2024-02-20 13:10:57 +0000

    chabnges done

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,8 @@
 console.log("hi sakshi  ");
+
+const  accId=43223
+let accEmail="sakshi@gmail.com"
+var accpass="1234"
+acccity="pune"
+//prefer using let to declare variables bcoz  of issue of block and functional scope
+console.table([accId,accEmail,accpass,acccity])
